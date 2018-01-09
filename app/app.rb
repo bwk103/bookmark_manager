4252c1014f2eb54d639107e0188141c9f8f50c09
Ben Kielty <bwk103@gmail.com>
2018-01-09 00:08:17 +0000

    Changed DB connection for heroku

--- app/app.rb
+++ app/app.rb
@@ -21,5 +21,4 @@ class BookmarkManager < Sinatra::Base
   get '/links/new' do
     erb(:'links/new')
   end
-
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -10,6 +10,6 @@ class Link
   property :url, String
 end
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'])
 DataMapper.finalize
 DataMapper.auto_upgrade!
